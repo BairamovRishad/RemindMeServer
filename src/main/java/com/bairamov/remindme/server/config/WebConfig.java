304f0c92f20e3992160c8f1303ea601b758c95fa
Rishad Bairamov <rbairamov@rightandabove.com>
2016-02-07 21:47:59 +0300

    Configure rest services.

--- src/main/java/com/bairamov/remindme/server/config/WebConfig.java
+++ src/main/java/com/bairamov/remindme/server/config/WebConfig.java
@@ -1,10 +1,17 @@
 package com.bairamov.remindme.server.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -13,5 +20,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        converters.add(converter);
     }
 }
